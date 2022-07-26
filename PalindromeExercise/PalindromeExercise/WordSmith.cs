590c7269955256e64c07dddc7e8002e87e6f3855
Stephen Wills <stephenrwills94@gmail.com>
2022-07-25 20:32:11 -0500

    finished main method

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string str)
+        {
+            var reversed = "";
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversed += str[i];
+            }
+            
+            
+            if(reversed == str)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,23 @@
-using System;
+﻿using System;
 using Xunit;
+using PalindromeExercise;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("racecar", true)] // ← you will need to put some test data and expected result here.  
+        [InlineData("hello", false)]
 
+        // For example [InlineData(“racecar”, true)]
+        public void Test1(string str, bool expected)
+        {
+            var test = new WordSmith();
+
+            bool actual = test.IsAPalindrome(str);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
